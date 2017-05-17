4b8cc15d2c1e5d137c55014286de951beaa7bcd1
Carlos de Souza Rodrigues Júnior <carlosrodriguesjunior@gmail.com>
2017-05-17 11:29:55 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('./src/config/mongoose');
 const mongodb = require('./src/config/mongodb')();
 
-console.log(mongodb.connection)
+console.log(mongodb.connection);
 
 module.exports = mongoose.connect(mongodb.connection, mongodb.options, err => {
     if (err) throw err;
